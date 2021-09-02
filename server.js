ed577a85aa84e57cd91604582b5ac9d69dad9f47
Rakib Ahmed <rakib987@gmail.com>
2021-09-01 21:17:16 -0400

    added public directory

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require(`express`);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(`public`));
+
+app.listen(PORT, () => {
+    console.info(`Example app listening at http://localhost:${PORT}`);
+})
